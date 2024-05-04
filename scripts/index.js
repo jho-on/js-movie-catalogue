ec2a5d0d072dea75c5060185847db36b85d34f91
Jhon <jhonatasousa288@gmail.com>
2024-05-04 12:35:01 -0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,65 @@
+let movieBlock = document.getElementById("movieBlock");
+
+function saveData(id){
+    console.log(id);
+    localStorage.setItem("clickedMovie", String(id));
+    location.href = "movieDetails.html"
+}
+
+async function fetchData(){
+    try{
+        const movieName = document.getElementById("movieName").value.toLowerCase();
+        localStorage.setItem("searchInput", movieName);
+        const response = await fetch("https://www.omdbapi.com/?apikey=c0dc0267&type=movie&s=" + movieName);
+            
+        if(!response.ok){
+            throw new Error("Could not fetch");
+        }
+
+        const data = await response.json();
+
+        if(data.Search != undefined){
+            movieBlock.innerHTML = "";
+            
+            for(let i = 0; i <= data.Search.length - 1; i++){
+                if(data.Search[i].Poster != "N/A"){
+                    movieBlock.insertAdjacentHTML("beforeend", 
+                    `
+                        <div id='movie'>
+                            <img onclick="saveData('${data.Search[i].imdbID}')" src='${data.Search[i].Poster}'>
+                            <h1>${data.Search[i].Title}</h1>
+                        </div>
+                    `);
+                }
+            }
+        }
+
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+
+document.addEventListener("keydown", function(e){
+    if(e.key == "Enter"){
+        document.getElementById("search").click();
+        document.getElementById("search").style.scale = 1.1;
+    }else{
+        document.getElementById("movieName").focus();
+        
+    }
+})
+
+document.addEventListener("keyup", function(e){
+    if(e.key == "Enter"){
+        document.getElementById("search").style.scale = 1;
+    }
+})
+
+window.onload = function(){
+    if(localStorage.getItem("searchInput") != undefined){
+        document.getElementById("movieName").value = localStorage.getItem("searchInput");
+        document.getElementById("search").click();
+    }
+}
---
+++ scripts/movieDetails.js
@@ -0,0 +1,19 @@
+async function fetchData(){
+    try{
+        const response = await fetch("https://www.omdbapi.com/?apikey=c0dc0267&type=movie&i=" + localStorage.getItem("clickedMovie") + '&plot=full');
+        const data = await response.json()
+        console.log(data)
+        document.getElementById('poster').src = data.Poster;
+        document.getElementById('movieTitle').textContent = data.Title;
+        document.getElementById('genre').textContent = data.Genre;
+        document.getElementById('moviePlot').textContent = data.Plot;
+        document.getElementById('imdb').textContent += data.Ratings[0].Value;
+        document.getElementById('rottenTomatoes').textContent += data.Ratings[1].Value;
+        document.getElementById('metaCritic').textContent += data.Ratings[2].Value;
+    }
+    catch(error){
+        console.error(error)
+    }
+}
+
+window.onload = fetchData();
